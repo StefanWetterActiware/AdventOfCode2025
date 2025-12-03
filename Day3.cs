278e02f8e311e53eab2af0d2621d03e081701d8d
StefanWe <31532767+StefanWetterActiware@users.noreply.github.com>
2025-12-03 07:43:31 +0100

    d3p1

--- Day3.cs
+++ Day3.cs
@@ -1,11 +1,25 @@
 
 class Day3 {
     internal static void doit(){
-        var input = Helper.getInput(int.Parse(Helper.dayNoR.Match(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name).Value));
-
+        var banks = Helper.getInputAsLines(int.Parse(Helper.dayNoR.Match(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name).Value));
+        
         long sumA = 0;
         long sumB = 0;
-        
+
+        foreach (var bank in banks)
+        {
+            for (int i = 99; i >= 0; i--)
+            {
+                int zehner = i / 10;
+                var einer = i % 10;
+                System.Text.RegularExpressions.Regex r = new System.Text.RegularExpressions.Regex(zehner + ".*" + einer);
+                if (r.IsMatch(bank))
+                {
+                    sumA += i;
+                    break;
+                }
+            }
+        }
 
         Console.WriteLine($"result: {sumA}");
         Console.WriteLine($"result B: {sumB}");
