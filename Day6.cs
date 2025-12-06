3839a05d82b1c3ed00533b62544e76a71b5dc923
StefanWe <31532767+StefanWetterActiware@users.noreply.github.com>
2025-12-06 19:49:37 +0100

    d6p2

--- Day6.cs
+++ Day6.cs
@@ -1,9 +1,9 @@
 class Day6 {
 
     internal static void doit() {
-        var input = Helper.getInputAsLines(int.Parse(Helper.dayNoR.Match(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name).Value));
+        var inputLines = Helper.getInputAsLines(int.Parse(Helper.dayNoR.Match(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name).Value));
 
-        var values = input.Select(line => line.Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)).ToList();
+        var values = inputLines.Select(line => line.Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)).ToList();
 
         long sumA = 0;
         long sumB = 0;
@@ -17,6 +17,49 @@ class Day6 {
                     break;
                 case "*":
                     sumA += nums.Aggregate(1L, (a,b) => a*b);
+                    break;
+            }
+        }
+
+        //Oh no ,Part2... Read it again from scratch
+        var calcers = inputLines.Last();
+        //calc start of columns
+        var colStartIndexes = new List<int>();
+        while (true) {
+            int idxP = calcers.IndexOf('+', colStartIndexes.Count == 0 ? 0 : colStartIndexes.Last() + 1);
+            int idxM = calcers.IndexOf('*', colStartIndexes.Count == 0 ? 0 : colStartIndexes.Last() + 1);
+
+            if (idxP + idxM == -2)
+                break;
+
+            if (idxP == -1) idxP = int.MaxValue;
+            if (idxM == -1) idxM = int.MaxValue;
+            int idx = Math.Min(idxP, idxM);
+
+            colStartIndexes.Add(idx);
+        }
+
+        var numLines = inputLines.Take(inputLines.Count() - 1);
+        for (int i = 0; i<colStartIndexes.Count(); i++) {
+            var colIdx = colStartIndexes[i];
+            var op = calcers[colIdx].ToString();
+            int width;
+            if  (i == colStartIndexes.Count() - 1)
+                width = inputLines.First().Length - colIdx;
+            else
+                width = colStartIndexes[i + 1] - colStartIndexes[i] -1;
+
+            List<long> lineNos = new();
+            for (int j = width -1; j>=0; j--) {
+                width--;
+                lineNos.Add(long.Parse(String.Join("", numLines.Select(a => a[colIdx + j])).Trim()));
+            }
+            switch (op) {
+                case "+":
+                    sumB += lineNos.Sum();
+                    break;
+                case "*":
+                    sumB += lineNos.Aggregate(1L, (a, b) => a * b);
                     break;
             }
         }
